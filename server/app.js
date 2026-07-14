154ae2df60a8fe90103489c4a50e87b7d503b617
amitkumarswami2023 <amit_dmarketing@sgtuniversity.org>
2026-07-14 13:55:41 +0530

    a

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import reportRoutes from "./routes/reportRoutes.js";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://your-project.vercel.app"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
@@ -27,5 +37,10 @@ app.get("/api/health", (req, res) => {
     server: "Running",
   });
 });
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/reports", reportRoutes);
 
 export default app;
